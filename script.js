ac0aa0e353828228a24960ac486e622b5cee1833
stevenpeterson1099 <59545506+stevenpeterson1099@users.noreply.github.com>
2023-08-15 16:09:36 -0400

    Update script.js
    Added script

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function reloadPage() {
 function changeText() {
     document.getElementById("demo").innerHTML = "Ramming Speed!"
 }
+function changeText2() {
+    document.getElementById("demo").innerHTML = "Code zero zero zero. Destruct. Zero."
+}
